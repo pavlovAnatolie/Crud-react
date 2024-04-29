d8a943dd3c9de415bbc1e70dcc22d714cc6339fd
pavlovAnatolie <127406685+pavlovAnatolie@users.noreply.github.com>
2024-04-29 14:37:25 +0200

    Update Alunno.js da controllare il funzionamento

--- app/src/Alunno.js
+++ app/src/Alunno.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 export default function Alunno({ alunno, popolaAlunni }) {
   const [inCancellazione, setInCancellazione] = useState(false);
   const [richiestaConferma, setRichiestaConferma] = useState(false);
+  const [Editing, setEditare] = useState(false);
+  const [nome, setNome] = useState("");
+  const [cognome, setCognome] = useState("");
 
   async function cancellaAlunno() {
     setRichiestaConferma(false);
@@ -22,12 +25,55 @@ export default function Alunno({ alunno, popolaAlunni }) {
     setRichiestaConferma(false);
   }
 
+  function avviaEditing() {
+    setEditing(true);
+  }
+
+   async function fineEditing() {
+    setEditing(false);
+     await fetch(`http://localhost:8080/alunni/${alunno.id}`, 
+        {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({nome: nome, cognome:cognome})
+    });
+
+     popolaAlunni();
+  }
+
+  function changeNome(event){
+        setNome(event.target.value);
+    }
+
+    function changeCognome(event){
+        setCognome(event.target.value);
+    }
+
   return (
     <tr>
-      <td>{alunno.nome}</td>
-      <td>{alunno.cognome}</td>
+    
+      <td>{Editing ? (
+       <input type="text" name="nome" onChange={changeNome}/><br />
+        </div>
+        ) : (
+          alunno.nome
+        )}</td>
+  
+      <td>{Editing ? (
+       <input type="text" name="nome" onChange={changeCognome}/><br />
+        </div>
+        ) : (
+          alunno.cognome
+        )}</td>
 
       <td>
+    {Editing ? (
+         <button onClick={fineEditing}>Salva</button>
+        ) : (
+          <button onClick={avviaEditing}>Edit</button>
+        )}
+
+    
         {richiestaConferma ? (
           <span>
             {" "}
